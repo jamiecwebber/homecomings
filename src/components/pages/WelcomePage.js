c5d87fee7b9e61b7cde3923b28908c5000d1d92c
jamiecwebber <jamiecwebber@gmail.com>
2020-08-24 17:27:51 -0400

    walkin a fine line here

--- src/components/pages/WelcomePage.js
+++ src/components/pages/WelcomePage.js
@@ -13,10 +13,10 @@ function WelcomePage () {
         <div>
             <h1>Welcome to Homecomings</h1>
             <h2>An art thing by harp+</h2>
-            <StyledTextBox>This website uses all sorts of classic, super-outdated web design stuff, and well, in this day and age you can't
-                just arbitrarily open a random number of popups whenever you would like to.
+            <StyledTextBox>This website uses all sorts of classic, super-outdated and ill-advised web design, and well, in this day and age you can't
+                just go around arbitrarily opening a random number of popups whenever you would like to.
             </StyledTextBox>
-            <StyledTextBox>Please click below and your browser will prompt you for a permissions for popups, cookies, etc., and then after that we can harass you with whatever ill-adviced web design decisions we may desire.
+            <StyledTextBox>If you're willing to go along with this for some reason, please click below and your browser will prompt you for a permissions for popups, cookies, etc., and then after that we can harass you with whatever horrific web design decisions we may desire.
             </StyledTextBox>
             <StyledTextBox>Hey at least you can't claim we weren't above-board with this
             </StyledTextBox>
