dac62d207719d7bef382eb139c5ee1eea24f48a4
jamiecwebber <jamiecwebber@gmail.com>
2020-08-24 18:58:10 -0400

    requesting popup permission

--- src/components/pages/WelcomePage.js
+++ src/components/pages/WelcomePage.js
@@ -14,6 +14,9 @@ const StyledTextBox = styled.div`
 const WelcomePage = ({permissionGranted, setPermissionGranted}) => {
 
     const requestPermission = async () => {
+        let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+            width=0,height=0,left=-1000,top=-1000`;
+        let popup = window.open("about:blank","test", params);
         setPermissionGranted(true);
     }
 
@@ -26,10 +29,11 @@ const WelcomePage = ({permissionGranted, setPermissionGranted}) => {
             </StyledTextBox>
             <StyledTextBox>If you're willing to go along with this for some reason, please click below and your browser will prompt you for permissions for popups, cookies, etc., and then after that we can harass you with whatever horrific web design decisions we may desire.
             </StyledTextBox>
+            <StyledTextBox>Please also disable any pop-up blockers or other extensions you think might get in the way. We promise that there is no malicious code or anything on this site, all we are is a bunch of humble musicians. Our intention is to annoy, not to actually cause any damage.
+            </StyledTextBox>
             <StyledTextBox>Hey at least you can't claim we weren't above-board with this
             </StyledTextBox>
-            <button onClick={requestPermission}>Yea that's cool</button>
-            {permissionGranted && <button><Link to="/main">Main</Link></button> }
+            {permissionGranted ? <button><Link to="/main">Main Page</Link></button> : <button onClick={requestPermission}>Yea that's cool</button> }
         </div>
     )
 }
