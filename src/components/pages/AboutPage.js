f3c8bb29f6b8e9bde69b168ddbd89a484ef8b34a
jamiecwebber <jamiecwebber@gmail.com>
2020-08-24 19:25:19 -0400

    added super basic background

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import aboutBackground from '../../media/timber.jpg'
+
+
+const Background = styled.div`
+
+    height: 100vh;
+    width: 100vw;
+    background-image: url(${aboutBackground});
+    background-position: 'center';
+    background-size: 100%;
+`
 
 function AboutPage () {
     return (
-        <div>
+        <Background>
             <div>
                 <h1>About this project</h1>
                 <div>canada council logo</div>
@@ -12,7 +24,7 @@ function AboutPage () {
                 </div>
             </div>
             <Link to='/main'>BACK TO MAIN PAGE</Link>
-        </div>
+        </Background>
     )
 }
 
--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,11 +1,26 @@
 import React from 'react'
-import NewWindow from 'react-new-window'
 import { Link } from 'react-router-dom'
+import homecomingsBackground from '../../media/homecomings4colours.jpg'
+import styled from 'styled-components';
+
+const Background = styled.div`
+
+    height: 100vh;
+    width: 100vw;
+    background-image: url(${homecomingsBackground});
+    background-position: 'center';
+    background-size: 100%;
+`
+
+const Title = styled.h1`
+    color: green;
+    font-size: 68px;
+`
 
 function MainPage () {
     return (
-        <div>
-            <h1>MAIN PAGE</h1>
+        <Background>
+            <Title>MAIN PAGE</Title>
             {/* <NewWindow name='blah1' features={{'width':200,'height':200}} >
                 <h1>HOMECOMINGS</h1>
             </NewWindow>
@@ -14,7 +29,7 @@ function MainPage () {
             </NewWindow> */}
             <Link to='/about'>ABOUT</Link><br/>
             <Link to='/'>back to welcome page</Link>
-        </div>
+        </Background>
     )
 }
 
